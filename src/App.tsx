b693ac45206598e1d6769b8f8505957730fe4450
MohammadHR10 <mhr9808@mavs.uta.edu>
2025-05-26 20:59:26 -0500

    Files added & Packages installed

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// Primary Router Component
+
+import './globals.css';
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello Snapgram!
+    </h1>
+  )
+}
+
+export default App
